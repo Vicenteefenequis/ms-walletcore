cd4392645072a668c81c61c984d8b763b4be737d
vicente_nascimento <vicente19981@live.com>
2023-01-16 22:03:28 -0300

    feat: create client and test

---
+++ internal/entity/client_test.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("Jhon doe", "j@j.com")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "Jhon doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestCreatedNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("Jhon doe", "j@j.com")
+
+	err := client.Update("Jhon Doe Update", "j@j.com")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Jhon Doe Update", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("Jhon doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+}
